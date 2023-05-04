4bd34feba77adaafde00ba26c53cd0d0172782c2
Iñigo Garcia Ugarte <garcia.127407@e.unavarra.es>
2023-05-04 12:12:36 +0200

    Commit #3

---
+++ src/screens/Reserva.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Reserva = () => {
+  return (
+    <View>
+      <Text>Esta es la pantalla Reserva</Text>
+    </View>
+  );
+};
+
+export default Reserva;
