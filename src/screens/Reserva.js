a24bbf1f136fead2909d3a4141e3522a58e88ce0
Iñigo Garcia Ugarte <garcia.127407@e.unavarra.es>
2023-05-25 18:57:24 +0200

    Merge 2

--- src/screens/Reserva.js
+++ src/screens/Reserva.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import { View, Text } from 'react-native';
-<<<<<<< HEAD
-=======
 import TablaReservas from '../components/TablaReservas';
->>>>>>> migration
 
 const Reserva = () => {
   return (
     <View>
-<<<<<<< HEAD
-      <Text>Esta es la pantalla Reserva</Text>
-=======
       <TablaReservas vehicle="car1" book_time="12"></TablaReservas>
->>>>>>> migration
     </View>
   );
 };
